2b7e5238a77ddeddb3f2e173f56ff21f7840b095
José Daniel Mantilla Franqui <danielfranqui016@gmail.com>
2023-10-16 10:48:02 -0500

    Corregidos errores en la consola sobre dimesiones de logo192.png y sobre props enviadas al DOM

--- src/componentes/Tarea.jsx
+++ src/componentes/Tarea.jsx
@@ -5,7 +5,7 @@ const TareaContenedor = styled.div`
     width: 500px;
     font-family: 'Reenie Beanie', cursive;
     min-height: 65px;
-    text-decoration: ${props => (props.completada === true ? 'line-through' : 'none')};
+    text-decoration: ${props => (props.completada === "true" ? 'line-through' : 'none')};
     margin: 5px 0;
     padding: 8px 15px 8px 20px;
     display: flex;
@@ -14,18 +14,18 @@ const TareaContenedor = styled.div`
     border-radius: 7px;
     border: 1px solid #3f7635;
     cursor: pointer;
-    color: ${props => (props.completada === true ? '#8b7200' : '#cff97c')};
+    color: ${props => (props.completada === "true" ? '#8b7200' : '#cff97c')};
 
     &:nth-child(3n + 1) {
-        background-color: ${props => (props.completada === true ? '#ffec83' : '#173d16')};
+        background-color: ${props => (props.completada === "true" ? '#ffec83' : '#173d16')};
     }
 
     &:nth-child(3n + 2) {
-        background-color: ${props => (props.completada === true ? '#ffec83' : '#2f752d')};
+        background-color: ${props => (props.completada === "true" ? '#ffec83' : '#2f752d')};
     }
 
     &:nth-child(3n + 3) {
-        background-color: ${props => (props.completada === true ? '#ffec83' : '#489145')};
+        background-color: ${props => (props.completada === "true" ? '#ffec83' : '#489145')};
     }
 `
 
@@ -51,7 +51,7 @@ const IconoEliminar = styled(FaDeleteLeft)`
 `
 
 const Tarea = ({ texto, completada, id, completarTarea, eliminarTarea }) => {
-    return <TareaContenedor completada={completada}>
+    return <TareaContenedor completada={completada.toString()}>
         <TareaTexto onClick={() => completarTarea(id)}>
             {texto}
         </TareaTexto>
